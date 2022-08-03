3eea5f1890f6725382df618add6a76088eb65cba
Olyazarichna <65617822+Olyazarichna@users.noreply.github.com>
2022-08-03 13:49:40 +0000

    Deploying to gh-pages from @ Olyazarichna/goit-react-hw-05-movies@eb16b3f376a81a84d38da6bbbd8387ca86002352 🚀

---
+++ static/js/805.109e19d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(501),s=n(184);t.default=function(){return(0,s.jsxs)("h2",{children:["Page not found",(0,s.jsx)("br",{}),(0,s.jsx)(r.rU,{to:"/",children:"Homepage"})]})}}}]);
+//# sourceMappingURL=805.109e19d5.chunk.js.map
